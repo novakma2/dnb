65dcbb2c584f843f70d0ff2adc4224a5f3d4d0ea
novak <novakma2@uhk.cz>
2019-03-09 15:34:50 +0100

    Init

---
+++ app/src/main/java/com/teamsix/dnbxchange/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.teamsix.dnbxchange
+
+import android.os.Bundle
+import android.util.JsonReader
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
