6fe8c106bce391a8ce437ee6bb71003436875a2d
Stefan <stefan.pankowiecki@openstax.pl>
2019-03-10 16:51:57 +0100

    ui something like V2

--- app/src/main/java/com/teamsix/dnbxchange/ExchangeActivity.kt
+++ app/src/main/java/com/teamsix/dnbxchange/ExchangeActivity.kt
@@ -4,21 +4,32 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_exchange.*
 
-class ExchangeActivity : Activity() {
+class ExchangeActivity : Activity(),AdapterView.OnItemSelectedListener {
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exchange)
-       // Spinner spiner  = clearFindViewById(R.id.countries_spiner)
+
+
+       
+
     }
+
 
     fun onClickMain(v: View){
 
@@ -29,6 +40,31 @@ class ExchangeActivity : Activity() {
 
 
 
+
+
     }
+
+}
+
+class Countries1 (val name: String, val iso: String, val path: String) {
+
+
+
+}
+
+fun main (parameter: Array <String>) {
+    val countries: Array <Countries> = arrayOf (
+        Countries ("Canada", "CA","canada_flag_48"),
+        Countries ("Czech Republic", "CZ","czech_republic_flag_48"),
+        Countries ("European Union", "  EU","european_union_flag_48"),
+        Countries ("Iceland", "IS","iceland_flag_48"),
+        Countries ("Japan", "JP","japan_flag_48"),
+        Countries ("Norway", "NO","norway_flag_48"),
+        Countries ("Poland", "PL","poland_flag_48"),
+        Countries ("Sweden", "SE","sweden_flag_48"),
+        Countries ("United Kingdom", "UK","united_kingdom_flag_48"),
+        Countries ("United States", "US","united_states_flag_48"))
+
+
 
 }
