b814d35bff9896fdf90dbaff0a660239450000c4
novak <novakma2@uhk.cz>
2019-03-21 15:19:55 +0100

    Amazon SDK, not working

---
+++ app/src/main/java/com/dnb/openbanking/gettingstarted/ErrorResponseHandler.java
@@ -0,0 +1,36 @@
+package com.dnb.openbanking.gettingstarted;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.http.HttpResponse;
+import com.amazonaws.http.HttpResponseHandler;
+import com.amazonaws.util.IOUtils;
+
+import java.io.IOException;
+
+public class ErrorResponseHandler implements HttpResponseHandler<AmazonServiceException> {
+
+    private boolean needsConnectionLeftOpen;
+
+    ErrorResponseHandler(final boolean needsConnectionLeftOpen) {
+        this.needsConnectionLeftOpen = needsConnectionLeftOpen;
+    }
+
+    @Override
+    public AmazonServiceException handle(final HttpResponse response) {
+        final AmazonServiceException ase = new AmazonServiceException(response.getStatusText());
+        ase.setStatusCode(response.getStatusCode());
+        try {
+            final String content = IOUtils.toString(response.getContent()).trim();
+            ase.setRawResponseContent(content);
+        } catch (IOException exception) {
+            System.err.println("Exception thrown while reading the response's content: " + exception);
+        }
+
+        return ase;
+    }
+
+    @Override
+    public boolean needsConnectionLeftOpen() {
+        return this.needsConnectionLeftOpen;
+    }
+}
